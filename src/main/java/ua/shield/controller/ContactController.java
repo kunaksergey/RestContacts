53056cf12cee62f676738008e9ad3f7643c10078
sa <kunaksergey@ukr.net>
2017-09-22 15:19:53 +0300

    22.09.2017 сhange Dao

--- src/main/java/ua/shield/controller/ContactController.java
+++ src/main/java/ua/shield/controller/ContactController.java
@@ -1,15 +1,16 @@
 package ua.shield.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ua.shield.entity.Contact;
 import ua.shield.service.ContactDao;
 
-import java.sql.SQLException;
 import java.util.List;
 
 
@@ -26,20 +27,17 @@ public class ContactController {
         this.contactDao = contactDao;
     }
 
-    @RequestMapping("/hello/contacts")
+    @RequestMapping(value = "/hello/contacts")
     public ResponseEntity<List<Contact>> contactList(@RequestParam(value = "nameFilter") final String pattern) {
-        List<Contact> allByFilter;
-        try {
-            allByFilter = contactDao.findAllByFilter(pattern);
+       try {
+            List<Contact> allByFilter = contactDao.findAllByFilter(pattern);
+            if ((allByFilter.size() > 0)) {
+                return new ResponseEntity<>(allByFilter, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         } catch (RuntimeException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        if ((allByFilter.size() > 0)) {
-            return new ResponseEntity<>(allByFilter, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
     }
-
- }
+}
